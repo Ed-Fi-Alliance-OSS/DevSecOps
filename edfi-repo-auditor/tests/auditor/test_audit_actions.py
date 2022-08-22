2944234bd640840909562d259a715f82bc40c130
AndonyNS <andony91@gmail.com>
2022-08-22 10:00:59 -0600

    Adding tests for individual action review

--- edfi-repo-auditor/tests/auditor/test_audit_actions.py
+++ edfi-repo-auditor/tests/auditor/test_audit_actions.py
@@ -16,36 +16,119 @@ CLIENT = GitHubClient(ACCESS_TOKEN)
 
 
 def describe_when_auditing_actions() -> None:
-    def it_returns_true_when_has_unit_tests() -> None:
-        ACTIONS = {
-            "total_count": 1,
-            "workflows": [{
-                "path": "test-action.yml"
-            }]
-        }
+    def describe_given_reviewing_codeql() -> None:
+        @pytest.fixture
+        def actions() -> dict:
+            return {
+                "total_count": 1,
+                "workflows": [{
+                    "path": "test-action.yml"
+                }]
+            }
 
-        ACTION_CONTENT = """
-            - name: Unit Tests with coverage
-        """
+        def it_returns_false_when_no_codeql(actions: dict) -> None:
+            file_content = """
+                - name: Analyze
+            """
+            CLIENT.get_actions = MagicMock(return_value=actions)
+            CLIENT.get_file_content = MagicMock(return_value=file_content)
+            results = audit_actions(CLIENT, OWNER, REPO)
+            assert results["Uses CodeQL"] is False
 
-        CLIENT.get_actions = MagicMock(return_value=ACTIONS)
-        CLIENT.get_file_content = MagicMock(return_value=ACTION_CONTENT)
-        results = audit_actions(CLIENT, OWNER, REPO)
-        assert results["Has Unit Tests"] is True
+        def it_returns_true_when_has_codeql(actions: dict) -> None:
+            file_content = """
+                - name: Analyze
+                  uses: github/codeql-action/analyze
+            """
+            CLIENT.get_actions = MagicMock(return_value=actions)
+            CLIENT.get_file_content = MagicMock(return_value=file_content)
+            results = audit_actions(CLIENT, OWNER, REPO)
+            assert results["Uses CodeQL"] is True
 
-    def it_returns_false_when_no_unit_tests() -> None:
-        ACTIONS = {
-            "total_count": 1,
-            "workflows": [{
-                "path": "test-action.yml"
-            }]
-        }
+    def describe_given_reviewing_allowed_list() -> None:
+        @pytest.fixture
+        def actions() -> dict:
+            return {
+                "total_count": 1,
+                "workflows": [{
+                    "path": "test-action.yml"
+                }]
+            }
 
-        ACTION_CONTENT = """
-            - name: Integration Tests
-        """
+        def it_returns_false_when_no_allowed_list(actions: dict) -> None:
+            file_content = """
+                - name: Scan
+                  uses: fake-action/allowed-list
+            """
+            CLIENT.get_actions = MagicMock(return_value=actions)
+            CLIENT.get_file_content = MagicMock(return_value=file_content)
+            results = audit_actions(CLIENT, OWNER, REPO)
+            assert results["Uses Allowed list"] is False
 
-        CLIENT.get_actions = MagicMock(return_value=ACTIONS)
-        CLIENT.get_file_content = MagicMock(return_value=ACTION_CONTENT)
-        results = audit_actions(CLIENT, OWNER, REPO)
-        assert results["Has Unit Tests"] is False
+        def it_returns_true_when_has_allowed_list(actions: dict) -> None:
+            file_content = """
+                - name: Scan
+                  uses: ed-fi-alliance-oss/ed-fi-actions/.github/workflows/repository-scanner.yml
+            """
+            CLIENT.get_actions = MagicMock(return_value=actions)
+            CLIENT.get_file_content = MagicMock(return_value=file_content)
+            results = audit_actions(CLIENT, OWNER, REPO)
+            assert results["Uses Allowed list"] is True
+
+    def describe_given_reviewing_test_reporter() -> None:
+        @pytest.fixture
+        def actions() -> dict:
+            return {
+                "total_count": 1,
+                "workflows": [{
+                    "path": "test-action.yml"
+                }]
+            }
+
+        def it_returns_false_when_no_test_reporter(actions: dict) -> None:
+            file_content = """
+                - name: Integration Tests
+            """
+            CLIENT.get_actions = MagicMock(return_value=actions)
+            CLIENT.get_file_content = MagicMock(return_value=file_content)
+            results = audit_actions(CLIENT, OWNER, REPO)
+            assert results["Uses Test Reporter"] is False
+
+        def it_returns_true_when_has_test_reporter(actions: dict) -> None:
+            file_content = """
+                - name: Integration Tests Report
+                uses: dorny/test-reporter
+            """
+            CLIENT.get_actions = MagicMock(return_value=actions)
+            CLIENT.get_file_content = MagicMock(return_value=file_content)
+            results = audit_actions(CLIENT, OWNER, REPO)
+            assert results["Uses Test Reporter"] is True
+
+    def describe_given_reviewing_unit_tests() -> None:
+        @pytest.fixture
+        def actions() -> dict:
+            return {
+                "total_count": 1,
+                "workflows": [{
+                    "path": "test-action.yml"
+                }]
+            }
+
+        def it_returns_true_when_has_unit_tests(actions: dict) -> None:
+            file_content = """
+                - name: Unit Tests with coverage
+            """
+
+            CLIENT.get_actions = MagicMock(return_value=actions)
+            CLIENT.get_file_content = MagicMock(return_value=file_content)
+            results = audit_actions(CLIENT, OWNER, REPO)
+            assert results["Has Unit Tests"] is True
+
+        def it_returns_false_when_no_unit_tests(actions: dict) -> None:
+            file_content = """
+                - name: Integration Tests
+            """
+            CLIENT.get_actions = MagicMock(return_value=actions)
+            CLIENT.get_file_content = MagicMock(return_value=file_content)
+            results = audit_actions(CLIENT, OWNER, REPO)
+            assert results["Has Unit Tests"] is False
