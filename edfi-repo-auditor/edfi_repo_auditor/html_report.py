1ad30c08ec121ad3fb6f6e18914bd5748a5c9690
Stephen A. Fuqua <stephen@safnet.com>
2022-11-17 09:16:07 -0600

    Reformat files

--- edfi-repo-auditor/edfi_repo_auditor/html_report.py
+++ edfi-repo-auditor/edfi_repo_auditor/html_report.py
@@ -3,6 +3,8 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
+# This is a stand-alone script for converting JSON output files to HTML
+
 from datetime import datetime
 import json
 import os
