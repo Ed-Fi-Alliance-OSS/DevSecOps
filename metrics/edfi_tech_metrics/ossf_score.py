2011fdfaae1f982d9b3063e0a68092bf3ca5c619
Christian <chrisgo@bu.edu>
2024-06-25 10:37:48 -0500

    Manual OSSF-Scoring Metrics

---
+++ metrics/edfi_tech_metrics/ossf_score.py
@@ -0,0 +1,34 @@
+import requests
+
+# Function to fetch OSSF score
+def get_ossf_score(organization, repository):
+    url = f"https://api.securityscorecards.dev/projects/github.com/{organization}/{repository}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data['score']
+    else:
+        return None
+
+# Function to report OSSF scores for multiple repositories
+def report_ossf_scores(repositories):
+    scores = {}
+    for repo in repositories:
+        organization, repository = repo.split('/')
+        score = get_ossf_score(organization, repository)
+        if score is not None:
+            scores[repo] = score
+        else:
+            scores[repo] = 'Error fetching score'
+    return scores
+
+# Function to fetch repositories for an organization
+def fetch_repositories(organization):
+    url = f"https://api.github.com/orgs/{organization}/repos"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        repositories = [repo['full_name'] for repo in data]
+        return repositories
+    else:
+        return []
